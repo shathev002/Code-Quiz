739599d00343163a7ea2f52dd49521ea3d7b2d61
Sharanya Thevakumaran <sharanyat@hotmail.com>
2023-05-24 17:07:41 -0400

    updated readme and cleaned code

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # Code-Quiz
+
+## Description
+
+Created a timed coding quiz using the HTML, CSS, and javaScipt concepts learned. 
+
+Used HTML to create the structure of the webpage and CSS to style it to make it look nice and responsive to the user.  
+
+Applied all the javaScript skills learned so far to create a quiz that functions with a timer and save scores into local storage. 
+
+Used variables to store different elements of the quiz to use them in functions needed to make the quiz interactive.
+
+Click events were used to allow the user to start the game, go to the next question, submit scores, reset scores, and return back to the start.
+
+To save scores entered into the game, utitlized local storage and JSON to convert the objects into values that can be displayed. 
+
+Used a for loop to run through and display scores entered into the score table. 
+
+Learned how to design and style a webpage that can interact with user with the use of javaScipt.
+
+
+## Installation
+
+N/A
+
+## Usage
+
+You can refer to the CSS, HTML and javaScript files to access the code for this project. 
+
+To view the actual webpage please refer to the following url link.
+
+https://shathev002.github.io/Password-Generator/
+
+You can also refer to the screenshot below. 
+
+![alt text](/code%20quiz%20screenshot.png)
+
+## Credits
+
+https://www.w3schools.com/ was used for reference regarding JavaScipt topics. 
+
+## License
+
+N/A
--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+// variables for the quiz 
+
 var startButton = document.querySelector("#startButton");
 var buttonOne = document.querySelector("#button1");
 var buttonTwo = document.querySelector("#button2");
@@ -20,194 +22,180 @@ var highScoreInput = document.querySelector(".highScoreInput");
 var backBtn = document.querySelector("#backBtn");
 var clearBtn = document.querySelector("#clearBtn");
 var time = 60;
-var timerInterval = '';
+var timerInterval = "";
 var currentPosition = 0;
 var score = 0;
-highScore.style.display = 'none'
-gameMenu.style.display = 'none'
-resultsMenu.style.display = 'none'
+highScore.style.display = "none";
+gameMenu.style.display = "none";
+resultsMenu.style.display = "none";
 
+// questions and answers stored in the variable questions
 
-var questions = [{
-  
-  question: "What does DOM stand for?",
-  answer: "Document Object Model",
-  options: ["Document Object Model", "Document Oriented Module", "Date of Birth", "Database Object Model"]
+var questions = [
+  {
+    question: "What does DOM stand for?",
+    answer: "Document Object Model",
+    options: [
+      "Document Object Model",
+      "Document Oriented Module",
+      "Date of Birth",
+      "Database Object Model",
+    ],
+  },
+  {
+    question:
+      "What type of loop continues through the code as long as the condition stated stays TRUE?",
+    answer: "While Loop",
+    options: ["For Loop", "While Loop", "Else Loop", "Conditional Loop"],
+  },
 
-},{
-  question: "What type of loop continues through the code as long as the condition stated stays TRUE?",
-  answer: "While Loop",
-  options: ["For Loop", "While Loop", "Else Loop", "Conditional Loop"]
+  {
+    question: "Which of the following is true about functions?",
+    answer: "All of the above",
+    options: [
+      "Code to be executed by the function is put inside curly brackets",
+      "The return statement stops the function from performing indefinitely",
+      "Local variables are variables declared within a function",
+      "All of the above",
+    ],
+  },
 
-}, 
+  {
+    question:
+      "What element in JavaScript is used to store multiple values in a variable?",
+    answer: "Arrays",
+    options: ["Functions", "Variables", "Arrays ", "Strings"],
+  },
 
-{question: "Which of the following is true about functions?",
-answer: "All of the above",
-options: ["Code to be executed by the function is put inside curly brackets", "The return statement stops the function from performing indefinitely", 
-"Local variables are variables declared within a function", "All of the above"]
+  {
+    question: "What format is used to store and transport data?",
+    answer: "JSON",
+    options: ["HTML", "Syntax", "For loop ", "JSON"],
+  },
+];
 
-}, 
+// click event for start button to start the game
 
-
-{question: "What element in JavaScript is used to store multiple values in a variable?",
-answer: "Arrays",
-options: ["Functions", "Variables", "Arrays ", "Strings"]
-
-}, 
-
-{question: "What format is used to store and transport data?",
-answer: "JSON",
-options: ["HTML", "Syntax", "For loop ", "JSON"]
-
-}
-
-]
-
-startButton.addEventListener("click",function(event){
+startButton.addEventListener("click", function (event) {
   event.preventDefault();
-  startMenu.style.display = 'none'
-  gameMenu.style.display = 'flex'
+  startMenu.style.display = "none";
+  gameMenu.style.display = "flex";
   setInterval(timerInterval);
-  questionText.innerText = questions[currentPosition].question
-  buttonOne.innerText = questions[currentPosition].options[0]
-  buttonTwo.innerText = questions[currentPosition].options[1]
-  buttonThree.innerText = questions[currentPosition].options[2]
-  buttonFour.innerText = questions[currentPosition].options[3]
-  
+  questionText.innerText = questions[currentPosition].question;
+  buttonOne.innerText = questions[currentPosition].options[0];
+  buttonTwo.innerText = questions[currentPosition].options[1];
+  buttonThree.innerText = questions[currentPosition].options[2];
+  buttonFour.innerText = questions[currentPosition].options[3];
+});
 
-})
+// click event for when user selects an option 
+// if correct answer chosen will increase score by one; if not time deducted by ten seconds 
+// if user reaches end of time or finishes all the questions, get to next section with scores or else go to the next question
 
-
-choices.addEventListener("click",function(event) {
-event.preventDefault();
+choices.addEventListener("click", function (event) {
+  event.preventDefault();
   if (event.target.innerText === questions[currentPosition].answer) {
     score += 1;
     if (currentPosition !== questions.length - 1 && time > 0) {
       nextQuestion();
-      console.log("line 101");
+      
     } else if (currentPosition === questions.length - 1 || time <= 0) {
       showScores();
-      console.log("line 104");
     }
-   //console.log("question: ", question);
-    console.log("score: ", score);
-  console.log("currentPosition: ", currentPosition);
-console.log("questions.length: ", questions.length);
-} else {
-  time-=10;
-  console.log("currentPosition: ", currentPosition);
-  console.log("questions.length: ", questions.length);
-  //console.log("question: ", question);
-  if (currentPosition !== questions.length - 1 && time > 0) {
-    nextQuestion();
-    console.log("line 115");
-  } else if (currentPosition === questions.length - 1 || time <= 0) {
-    showScores();
-    console.log("line 117");
-  }
-  console.log("incorrect");
-} 
-
-}
-
-);
-
-
-function nextQuestion(){
-  currentPosition++;
- 
-  //rewrite h1 and choices with question[currentPosition]
-  questionText.innerText = questions[currentPosition].question
-  buttonOne.innerText = questions[currentPosition].options[0]
-  buttonTwo.innerText = questions[currentPosition].options[1]
-  buttonThree.innerText = questions[currentPosition].options[2]
-  buttonFour.innerText = questions[currentPosition].options[3]
   
+  } else {
+    time -= 10;
+   
+    if (currentPosition !== questions.length - 1 && time > 0) {
+      nextQuestion();
+      
+    } else if (currentPosition === questions.length - 1 || time <= 0) {
+      showScores();
+    }
+   
+  }
+});
 
-};
+function nextQuestion() {
+  currentPosition++;
+
+  //rewrite h1 and choices with question[currentPosition]
+  questionText.innerText = questions[currentPosition].question;
+  buttonOne.innerText = questions[currentPosition].options[0];
+  buttonTwo.innerText = questions[currentPosition].options[1];
+  buttonThree.innerText = questions[currentPosition].options[2];
+  buttonFour.innerText = questions[currentPosition].options[3];
+}
 
 //timer
 
 timerInterval = setInterval(countDown, 1000);
 
-function countDown () {
-   timer.innerText = time +" seconds";
-if (time <= 60) {
-  time--;
-} else if (currentPosition === questions.length - 1 || time <= 0) {
-  clearInterval(timerInterval);
-  showScores();
+function countDown() {
+  timer.innerText = time + " seconds";
+  if (time <= 60) {
+    time--;
+  } else if (currentPosition === questions.length - 1 || time <= 0) {
+    clearInterval(timerInterval);
+    showScores();
+  }
 }
-
-};
 
 //scores
-function showScores () {
-  gameMenu.style.display = 'none'
-  resultsMenu.style.display = 'flex'
+
+function showScores() {
+  gameMenu.style.display = "none";
+  resultsMenu.style.display = "flex";
   myScore.innerText = `Your score: ${score}`;
-
 }
 
-submitScore.addEventListener("click", function() {
-  resultsMenu.style.display = 'none'
-  highScore.style.display = 'flex'
+// click event for when user submits initials and saves score 
+//created for loop to display each score entered into the game and added them to the table element 
+
+submitScore.addEventListener("click", function () {
+  resultsMenu.style.display = "none";
+  highScore.style.display = "flex";
   saveScore();
-  
+
   for (let i = 0; i < highScores.length; i++) {
-  var row = document.createElement('tr');
-  var rowTwo = document.createElement('tr');
-  console.log(highScores[i].score);
-
-  highScoreName.textContent = "Name: " + highScores[i].name;
-  highScoreInput.textContent = "Score: " + highScores[i].score;
-  row.innerHTML=highScoreName.textContent;
-  rowTwo.innerHTML=highScoreInput.textContent;
-  document.getElementsByTagName('table')[0].appendChild(row);
-  document.getElementsByTagName('table')[0].appendChild(rowTwo);
- 
-}
-  
-
+    var row = document.createElement("tr");
+    var rowTwo = document.createElement("tr");
+    
+    highScoreName.textContent = "Name: " + highScores[i].name;
+    highScoreInput.textContent = "Score: " + highScores[i].score;
+    row.innerHTML = highScoreName.textContent;
+    rowTwo.innerHTML = highScoreInput.textContent;
+    document.getElementsByTagName("table")[0].appendChild(row);
+    document.getElementsByTagName("table")[0].appendChild(rowTwo);
+  }
 });
 
-function saveScore () {
-  
+function saveScore() {
   var finalScore = {
     name: scoreName.value,
-    score: score
-
+    score: score,
   };
-  console.log("finalScore", finalScore, typeof finalScore); 
-  highScores.push(finalScore);  
-  console.log("highScores:", highScores); 
+  
+  highScores.push(finalScore);
+  
   localStorage.setItem("scores", JSON.stringify(highScores));
 }
 
-// return to start menu
+// return to start menu 
 
-
-for (let i = 0; i < highScores.length; i++) {
-  console.log(highScores[i]);
-  highScoreName.textContent = "Name: " + highScores[i].name;
-  highScoreInput.textContent = "Score: " + highScores[i].score;
-}
-
-
-backBtn.addEventListener("click", function(event) {
+backBtn.addEventListener("click", function (event) {
   event.preventDefault();
-  highScore.style.display = 'none'
-  startMenu.style.display = 'flex'
+  highScore.style.display = "none";
+  startMenu.style.display = "flex";
   time = 60;
   currentPosition = 0;
-
-  
+  score = 0;
 });
 
+// clear scores from local storage
 
-clearBtn.addEventListener("click", function(event) {
+clearBtn.addEventListener("click", function (event) {
   event.preventDefault();
   score = 0;
-localStorage.removeItem("scores");
-
+  localStorage.removeItem("scores");
 });
